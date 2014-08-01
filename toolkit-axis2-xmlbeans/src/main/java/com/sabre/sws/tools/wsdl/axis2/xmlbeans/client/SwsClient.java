47c3b39d1e32b222c3ebbde6b86a10818371ceb2
SG0221139 <jedrzej.dabrowa@sabre.com>
2014-08-01 14:09:09 +0200

    POM + docs

--- toolkit-axis2-xmlbeans/src/main/java/com/sabre/sws/tools/wsdl/axis2/xmlbeans/client/SwsClient.java
+++ toolkit-axis2-xmlbeans/src/main/java/com/sabre/sws/tools/wsdl/axis2/xmlbeans/client/SwsClient.java
@@ -3,7 +3,9 @@ package com.sabre.sws.tools.wsdl.axis2.xmlbeans.client;
 import com.sabre.sws.tools.wsdl.axis2.xmlbeans.wrappers.SessionCloseWrapper;
 import com.sabre.sws.tools.wsdl.axis2.xmlbeans.wrappers.SessionCreateWrapper;
 import com.sabre.sws.tools.wsdl.commons.handlers.MustUnderstandHandler;
+import com.sabre.sws.tools.wsdl.commons.handlers.OutputHandler;
 import com.sabre.sws.tools.wsdl.commons.utils.IConfigurationProvider;
+import com.sabre.sws.tools.wsdl.commons.utils.SessionManager;
 import com.sabre.sws.tools.wsdl.commons.utils.Util;
 import org.opentravel.www.ota._2002._11.SessionCreateRSDocument;
 
@@ -21,6 +23,10 @@ public class SwsClient {
     public static void main( String ... args ) throws RemoteException {
 
         SessionCreateWrapper stub = new SessionCreateWrapper( configuration );
+
+        List hnd = new ArrayList();
+        hnd.add( new OutputHandler() );
+        hnd.add( new MustUnderstandHandler() );
 
         stub._getServiceClient().getAxisConfiguration().setInPhasesUptoAndIncludingPostDispatch( hnd );
         SessionCreateRSDocument document = stub.openSession();
