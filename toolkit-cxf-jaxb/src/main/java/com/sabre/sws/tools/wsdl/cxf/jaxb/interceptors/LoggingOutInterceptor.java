90d3e77d80e54c77c9d8b0b07374150358c011de
SG0221139 <jedrzej.dabrowa@sabre.com>
2014-09-18 17:03:25 +0200

    Refactoring

--- toolkit-cxf-jaxb/src/main/java/com/sabre/sws/tools/wsdl/cxf/jaxb/interceptors/LoggingOutInterceptor.java
+++ toolkit-cxf-jaxb/src/main/java/com/sabre/sws/tools/wsdl/cxf/jaxb/interceptors/LoggingOutInterceptor.java
@@ -1,10 +1,10 @@
 package com.sabre.sws.tools.wsdl.cxf.jaxb.interceptors;
 
 import com.sabre.sws.tools.wsdl.commons.utils.XMLPrettifier;
-import com.sabre.sws.tools.wsdl.cxf.jaxb.utils.SoapMessageUtils;
+import org.apache.commons.io.IOUtils;
 import org.apache.cxf.binding.soap.SoapMessage;
 import org.apache.cxf.binding.soap.interceptor.AbstractSoapInterceptor;
-import org.apache.cxf.binding.soap.interceptor.SoapActionInInterceptor;
+import org.apache.cxf.binding.soap.interceptor.SoapPreProtocolOutInterceptor;
 import org.apache.cxf.interceptor.Fault;
 import org.apache.cxf.io.CachedOutputStream;
 import org.apache.cxf.phase.Phase;
@@ -12,6 +12,8 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 /**
  * Created by SG0221139 on 9/11/2014.
@@ -21,25 +23,50 @@ import java.io.IOException;
  */
 public class LoggingOutInterceptor extends AbstractSoapInterceptor {
 
-    private static final Logger LOGGER = LogManager.getLogger( LoggingInInterceptor.class );
+    private static final Logger LOGGER = LogManager.getLogger( LoggingOutInterceptor.class );
 
     public LoggingOutInterceptor() {
-        super( Phase.SEND );
-        addAfter(SoapActionInInterceptor.class.getName());
+            super(Phase.PRE_STREAM);
+            addBefore(SoapPreProtocolOutInterceptor.class.getName());
+
     }
 
     @Override
     public void handleMessage(SoapMessage message) throws Fault {
+
         try {
             logMessageContent(message);
         } catch (IOException e) {
-            throw new Fault(e);
+            logAndRethrow(e);
         }
     }
 
+    private void logAndRethrow(IOException e) {
+        LOGGER.error( e );
+        throw new RuntimeException(e);
+    }
+
     private void logMessageContent(SoapMessage message) throws IOException {
-        CachedOutputStream messageCopyStream = SoapMessageUtils.copyMessageIntoStream(message);
-        String formattedMessage = XMLPrettifier.pretify(messageCopyStream.getInputStream());
-        LOGGER.info( formattedMessage );
+
+        OutputStream originalMessageOutputStream = message.getContent(OutputStream.class);
+
+        message.setContent(OutputStream.class, new CachedOutputStream());
+        message.getInterceptorChain().doIntercept(message);
+
+        CachedOutputStream csnew = (CachedOutputStream) message.getContent(OutputStream.class);
+        String currentEnvelopeMessage = IOUtils.toString(csnew.getInputStream(), "UTF-8");
+        csnew.flush();
+        IOUtils.closeQuietly(csnew);
+        InputStream replaceInStream = IOUtils.toInputStream(currentEnvelopeMessage, "UTF-8");
+
+        IOUtils.copy(replaceInStream, originalMessageOutputStream);
+        IOUtils.closeQuietly(replaceInStream);
+
+        String formattedLogMessage = XMLPrettifier.pretify(currentEnvelopeMessage);
+        LOGGER.info( formattedLogMessage );
+
+        originalMessageOutputStream.flush();
+        message.setContent(OutputStream.class, originalMessageOutputStream);
+        IOUtils.closeQuietly(originalMessageOutputStream);
     }
 }
